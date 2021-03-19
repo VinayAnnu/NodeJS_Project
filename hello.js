8b6a61d1bc2a0851d6b5281d3489a5ab66fc29e9
Vinay kumar ranjan <vinaykumarranjan83@gmail.com>
2021-03-19 14:42:47 +0530

    commit settime

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
 console.log("hello git");
+
+setTimeout(() => {
+    console.log("10ms");
+}, 10);
+
+setTimeout(()=>{
+    console.log("5ms");
+},5)
+setTimeout(()=>{
+    console.log("5ms 2nd");
+},5)
+console.log("welcome");
